9b23f87ebe5c93642443c71b91ad9ec85470eb48
EC2 Default User <ec2-user@ip-172-31-31-241.ec2.internal>
2021-02-26 23:46:22 +0000

    Initial Setup

---
+++ App_Hw9.js
@@ -0,0 +1,15 @@
+import { useState, useRef } from 'react';
+import './App.css';
+import './Board.css';
+import {Board} from './Board.js';
+import logo from './logo.svg';
+
+
+function App() {
+  return(
+  <Board />
+ );
+}
+
+export default App;
+
