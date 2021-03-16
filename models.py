3333b21d1157efa1dfe4a6b8976b1be38cf22b92
EC2 Default User <ec2-user@ip-172-31-31-241.ec2.internal>
2021-03-16 19:03:55 +0000

    Applied pylint to models.py file

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
+'''This file is used to create,enter,get data from database'''
+# pylint: disable= E1101, C0413, R0903, W0603, W1508
 from app import DB
 
-
 class Person(DB.Model):
+    '''The person class is used to store and retrieve data from database'''
     username = DB.Column(DB.String(80),
                          primary_key=True,
                          unique=True,
